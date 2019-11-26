04a14bd390c9cfcd4d778e443d8ed80c4bc734f9
attiq Ur-Rehman <attiq.urrehman@aptos.com>
2019-11-26 14:44:36 +0000

    qq

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ function authenticateWithAWS()
 
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
+    console.log(data);
   });
 
 }).on("error", (err) => {
